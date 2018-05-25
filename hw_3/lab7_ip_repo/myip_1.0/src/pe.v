ae98f290c3b7e8ccaecf688003ff64f82ccf938e
Jaeseong Seo <07js23@gmail.com>
2018-05-25 17:00:40 +0900

    Convert big 16 to little 32

--- hw_3/lab7_ip_repo/myip_1.0/src/pe.v
+++ hw_3/lab7_ip_repo/myip_1.0/src/pe.v
@@ -39,13 +39,28 @@ module my_pe #(parameter L_RAM_SIZE = 6)
       else
         dout_fb <= dout_fb;
 
+  // Convert big endian to little endian
+  wire little_ain[31:0];
+  wire little_bin[31:0];
+  assign little_ain = { ain[7:0], ain[15:8], ain[23:16], ain[31:24] };
+  assign little_bin = { bin[7:0], bin[15:8], bin[23:16], bin[31:24] };
+  // Convert 16-bit floats to 32-bit floats
+  wire ain_converted[31:0];
+  wire bin_converted[31:0];
+  assign ain_converted = ((little_ain & 0x8000) << 16)
+                            | ((((little_ain & 0x7C00) >> 10) + 112) << 23)
+                            | ((little_ain & 0x3FF) << 13);
+  assign bin_converted = ((little_bin & 0x8000) << 16)
+                            | ((((little_bin & 0x7C00) >> 10) + 112) << 23)
+                            | ((little_bin & 0x3FF) << 13);
+
   floating_point_0 u_float_dsp (
     .aclk             (aclk),
     .aresetn          (aresetn),
     .s_axis_a_tvalid  (avalid),
-    .s_axis_a_tdata   (ain),
+    .s_axis_a_tdata   (ain_converted),
     .s_axis_b_tvalid  (bvalid),
-    .s_axis_b_tdata   (bin),
+    .s_axis_b_tdata   (bin_converted),
     .s_axis_c_tvalid  (cvalid),
     //.s_axis_c_tdata   (sreg[FLOATDELAY-1]),
     .s_axis_c_tdata   (dout_fb),
