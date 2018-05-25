d8f1c0ffa91f495a1e47c67b78d9757cef708f4f
Your Name <uk6342@gmail.com>
2018-05-25 17:19:10 +0900

    Fix misplaced token

--- hw_3/lab7_ip_repo/myip_1.0/src/pe.v
+++ hw_3/lab7_ip_repo/myip_1.0/src/pe.v
@@ -40,19 +40,20 @@ module my_pe #(parameter L_RAM_SIZE = 6)
         dout_fb <= dout_fb;
 
   // Convert big endian to little endian
-  wire little_ain[31:0];
-  wire little_bin[31:0];
+  wire [31:0] little_ain;
+  wire [31:0] little_bin;
   assign little_ain = { ain[7:0], ain[15:8], ain[23:16], ain[31:24] };
   assign little_bin = { bin[7:0], bin[15:8], bin[23:16], bin[31:24] };
   // Convert 16-bit floats to 32-bit floats
-  wire ain_converted[31:0];
-  wire bin_converted[31:0];
-  assign ain_converted = ((little_ain & 0x8000) << 16)
-                            | ((((little_ain & 0x7C00) >> 10) + 112) << 23)
-                            | ((little_ain & 0x3FF) << 13);
-  assign bin_converted = ((little_bin & 0x8000) << 16)
-                            | ((((little_bin & 0x7C00) >> 10) + 112) << 23)
-                            | ((little_bin & 0x3FF) << 13);
+  wire [31:0] ain_converted;
+  wire [31:0] bin_converted;
+  
+  assign ain_converted = ((little_ain & 32'h8000) << 16)
+                            | ((((little_ain & 32'h7C00) >> 10) + 112) << 23)
+                            | ((little_ain & 32'h3FF) << 13);
+  assign bin_converted = ((little_bin & 32'h8000) << 16)
+                            | ((((little_bin & 32'h7C00) >> 10) + 112) << 23)
+                            | ((little_bin & 32'h3FF) << 13);
 
   floating_point_0 u_float_dsp (
     .aclk             (aclk),
